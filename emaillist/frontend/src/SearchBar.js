bbb3954e55c8a8c7ac3c69a2f56dcf9cc79d5e93
kimki0917 <kimki0917@naver.com>
2023-03-21 16:26:51 +0900

    plz

---
+++ emaillist/frontend/src/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './assets/scss/SearchBar.scss';
+
+export default function SearchBar({callback}) {
+  return (
+    <div className={styles.Searchbar}>
+        <input
+          type='text'
+          placeholder='찾기'
+          onChange={e => callback(e.target.value)}/>
+    </div>
+  )
+}
+//제어컴포넌트 안만들어도 됨
+//바로 호출할 수 있게
+
+//plz
